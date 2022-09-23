b5d799e0f72ca91022b859b0e8484a69149a9cbf
Max Hur <109092333+doublefriedtofu@users.noreply.github.com>
2022-09-23 16:05:30 +0900

    Max's odd Checker

---
+++ odd.js
@@ -0,0 +1,10 @@
+let isOdd = function(i){
+  if (i % 2 !== 0){
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
